ab35b43d430b9ed49275465b2fb7879f76eabd28
awing256 <videomanoh@gmail.com>
2025-04-03 12:35:35 -0500

    adding images for products

--- cozy-threads/src/data/catalogData.ts
+++ cozy-threads/src/data/catalogData.ts
@@ -4,35 +4,35 @@ export const catalogData: ProductItem[] = [
     {
         id: 0,
         title: "Cotton Linen Open Collar Shirt",
-        image: "image01.png",
+        image: "image01.jpg",
         description: "57% Cotton, 43% Linen",
         price: 29.49
     },
     {
         id: 1,
         title: "Genuine Leather Jacket",
-        image: "image02.png",
+        image: "image02.jpg",
         description: "Squared shoulders and flat linings on all sides, great for that Daredevil feel",
         price: 52.39
     },
     {
         id: 2,
-        title: "Ultra Stretch Pants",
-        image: "image03.png",
-        description: "Breathable and classy pants",
-        price: 39.99
+        title: "OSU Sports Jersey",
+        image: "image03.jpg",
+        description: "Go Bucks!!!",
+        price: 79.99
     },
     {
         id: 3,
-        title: "Off-White Cardigan",
-        image: "image04.png",
-        description: "Ringing in the springtime breeze with this loose fitting sweater",
+        title: "Off-White Knit Shirt",
+        image: "image04.jpg",
+        description: "Really really off white, great for dancing in",
         price: 59.99
     },
     {
         id: 4,
         title: "Blue Outdoor Rain Jacket",
-        image: "image05.png",
+        image: "image05.jpg",
         description: "Water resistant, plenty of indoor and outdoor zipper pockets",
         price: 29.99
     }
