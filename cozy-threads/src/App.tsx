34b55844df170291d38e9f5e5b2324892ead35dc
awing256 <videomanoh@gmail.com>
2025-04-03 12:35:26 -0500

    routing to separate pages

--- cozy-threads/src/App.tsx
+++ cozy-threads/src/App.tsx
@@ -1,13 +1,23 @@
 import './App.css'
-import {CatalogList} from "./components/Catalog/CatalogList.tsx";
 import {CartProvider} from "./components/ShoppingCart/CartContextProvider.tsx";
+import {CartDisplay} from "./components/ShoppingCart/CartDisplay.tsx"
+import {BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import {Container} from "@mui/material";
+import { CatalogList } from './components/Catalog/CatalogList.tsx';
 
 function App() {
 
   return (
     <div>
         <CartProvider>
-            <CatalogList/>
+            <Router>
+                <Container maxWidth="lg">
+                    <Routes>
+                        <Route path="/" element={<CatalogList />} />
+                        <Route path="/cart" element={<CartDisplay />} />
+                    </Routes>
+                </Container>
+            </Router>
         </CartProvider>
     </div>
   )
--- cozy-threads/src/components/Catalog/CatalogList.tsx
+++ cozy-threads/src/components/Catalog/CatalogList.tsx
@@ -1,9 +1,17 @@
 import {catalogData} from "../../data/catalogData.ts";
 import {CatalogCard} from "./CatalogCard.tsx";
+import {IconButton, Toolbar} from "@mui/material";
+import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
+import { Link } from "react-router-dom";
 
 export const CatalogList = () => {
     return(
         <div>
+            <Toolbar>
+                <IconButton component={Link} aria-label="shopping cart" to="/cart">
+                    <ShoppingCartIcon  />
+                </IconButton>
+            </Toolbar>
             {catalogData.map(product=>
                 <CatalogCard product={product}/>
             )}
