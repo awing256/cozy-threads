3fe11479e9fd9807e23e7f54f3c5b0c18b438eda
awing256 <videomanoh@gmail.com>
2025-04-04 07:57:37 -0500

    server endpoint to send payment intent and return clientSecret

--- cozy-threads/server/server.ts
+++ cozy-threads/server/server.ts
@@ -1,36 +1,30 @@
-// This is your test secret API key.
-const stripe = require("stripe")(
-    process.env.STRIPE_SECRET_KEY, {
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY!, {
     apiVersion: "2025-03-31.basil",
 });
+
 const express = require("express");
 const app = express();
 const cors = require('cors')
-app.use(cors({origin: 'http://lcoalhost:5500'}))
+app.use(cors({origin: 'http://localhost:5173'}))
+app.use(express.json());
 
-const YOUR_DOMAIN = "http://localhost:3000";
-
-app.post("/create-checkout-session", async (req, res) => {
+app.post('/create-payment-intent', async (req, res) => {
+    // console.log('Request body:', req.body);
     try {
-        const session = await stripe.checkout.sessions.create({
-            ui_mode: "custom",
-            line_items: [
-                {
-                    // Provide the exact Price ID (e.g. pr_1234) of the product you want to sell
-                    price: "{{PRICE_ID}}",
-                    quantity: 1,
-                },
-            ],
-            mode: "payment",
-            success_url: `${process.env.CLIENT_URL}/success.html`,
-            cancel_url: `${process.env.CLIENT_URL}/cancel.html`
-
+        const { amount } = req.body;
+        const paymentIntent = await stripe.paymentIntents.create({
+            amount: amount,
+            currency: 'usd',
         });
-        res.json({url: session.url})
-    }catch(e){
-        res.status(500).json({error:e.message})
-    }
 
+        res.json({ clientSecret: paymentIntent.client_secret });
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
 });
 
 app.listen(3000, () => console.log("Running on port 3000"));
