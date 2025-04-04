394434dbb72190142e33f6a63028e9aaae9dd425
awing256 <videomanoh@gmail.com>
2025-04-04 15:55:49 -0500

    cors origin allow

--- server/server.js
+++ server/server.js
@@ -9,10 +9,7 @@ const app = express();
 
 
 app.use(cors({
-    origin: [
-        'http://localhost:5173',
-        'https://cozy-threads-frontend.onrender.com',
-    ],
+    origin: 'https://cozy-threads-frontend.onrender.com',
     credentials: true,
     methods: ['POST'],
     allowedHeaders: ['Content-Type', 'Authorization']
