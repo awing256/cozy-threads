96856dba267b6ada770b8b112a213456f73ecaff
awing256 <videomanoh@gmail.com>
2025-04-03 22:05:02 -0500

    cart add checkout button

--- cozy-threads/src/components/ShoppingCart/CartDisplay.tsx
+++ cozy-threads/src/components/ShoppingCart/CartDisplay.tsx
@@ -1,8 +1,11 @@
 import {useCart} from "./useCart.tsx";
 import {CartCard} from "./CartCard.tsx";
 import DeleteIcon from '@mui/icons-material/Delete';
+import ShoppingCartCheckoutIcon from '@mui/icons-material/ShoppingCartCheckout';
 import {Button, Typography} from '@mui/material';
 import SiteHeader from "../SiteHeader.tsx";
+import Toolbar from "@mui/material/Toolbar";
+import {Link} from "react-router-dom";
 
 export const CartDisplay = () => {
     const {clearCart, cartItems, getTotalCost} = useCart();
@@ -13,17 +16,33 @@ export const CartDisplay = () => {
             {cartItems.map(cartItem=>
                 <CartCard cartItem={cartItem}/>
             )}
-            <Button
-                variant="outlined"
-                color="error"
-                startIcon={<DeleteIcon />}
-                onClick={clearCart}
-            >
-                Clear All Items From Cart
-            </Button>
-            <Typography gutterBottom variant="h5" component="div">
-                Total Cost of Cart: ${getTotalCost()}
-            </Typography>
+            <Toolbar sx={{
+                display: 'flex',
+                justifyContent: 'space-between',
+                width: '100%'
+            }}>
+                <Typography gutterBottom variant="h5" component="div">
+                    Total Cost of Cart: ${getTotalCost()}
+                </Typography>
+                <Button
+                    variant="outlined"
+                    color="error"
+                    startIcon={<DeleteIcon />}
+                    onClick={clearCart}
+                >
+                    Clear All Items From Cart
+                </Button>
+                <Button
+                    variant="outlined"
+                    color="success"
+                    startIcon={<ShoppingCartCheckoutIcon  />}
+                    to='/checkout'
+                    component={Link}
+                >
+                    Checkout
+                </Button>
+
+            </Toolbar>
         </>
     )
 
